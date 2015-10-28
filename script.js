d0946cc2f3ff113999a371ca47549cb2b6423b02
CTwitchell <YellingAtFish@gmail.com>
2015-10-28 10:23:30 -0600

    Fixed the submit button. Working on keypress function.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function submitName() {
-   var playername1 = document.getElementById("name");
-    if(playername1.value !== "") {
-        alert (playername2);
+//var entersubmit = keyCode == 13;
+var nameSubmit = function () {
+    if (document.getElementById("userinput").value !== "") {
+        document.getElementById("name1").innerHTML = document.getElementById("userinput").value;
+        document.getElementById("userinput").value = "";
+    } else {
+        alert("Please Enter A Name.");
     }
-}
+};
+
+document.getElementById("submit").onclick = nameSubmit;
+//document.getElementById("submit").submit = nameSubmit;
+
+//var charfield=document.getElementById("char")
+//charfield.onkeydown=function(e){
+//var e=window.event || e;
+//var playerName = document.getElementById("userinput").value
+//document.getElementById("keycode").value=e.keyCode
+//}
