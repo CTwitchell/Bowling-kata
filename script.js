735eb8d34c89f9c1c07f2cb760854d5ab0cc9223
CTwitchell <YellingAtFish@gmail.com>
2015-11-03 10:18:28 -0700

    Attempt at adding the scoring code.

--- script.js
+++ script.js
@@ -22,17 +22,67 @@ function entInfo(e) {
 }
 
 document.getElementById("submit").onclick = nameSubmit;
-var i = 1;
+//var i = 1;
 
-function rolls(e) {
+//function rolls(e) {
+//
+//    if (i <= 20 && e !== 10) {
+//        document.getElementById("roll" + i).innerHTML = e;
+//        i++;
+//    }
+//    if (i % 2 !== 0 && e == 10) {
+//        i++;
+//        document.getElementById("roll" + i).innerHTML = "X";
+//        i++;
+//    }
+//}
 
-    if (i <= 20 && e !== 10) {
-        document.getElementById("roll" + i).innerHTML = e;
-        i++;
+
+var BowlingGame = [];
+
+BowlingGame.roll = function (pins) {
+    BowlingGame.push (pins);
+};
+
+BowlingGame.score = function () {
+    var result = 0;
+    var rollIndex = 0; 
+    var game = this;
+    for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
+        if (isStrike()){
+            result += getStrikeScore();
+            documnet.getElementById("roll" + rollIndex).innerHTML = BowlingGame[0];
+            rollIndex++;
+            
+        
+        } else if (isSpare()){
+            result += getSpareScore();
+            documnet.getElementById("roll" + rollIndex).innerHTML = BowlingGame[rollIndex];
+            rollIndex +=2;
+        } else {
+        result += getNormalScore();
+        documnet.getElementById("roll" + rollIndex).innerHTML = BowlingGame[rollIndex];
+        rollIndex +=2;
+        }
+     }
+    
+    return result;
+    
+    function isSpare(){
+        return game.rolls[rollIndex] + game.rolls[rollIndex + 1] ==10;
     }
-    if (i % 2 !== 0 && e == 10) {
-        i++;
-        document.getElementById("roll" + i).innerHTML = "X";
-        i++;
+    
+    function isStrike(){
+        return game.rolls[rollIndex] == 10;
     }
-}
+    
+   function getStrikeScore() {
+        return game.rolls[rollIndex] + game.rolls [rollIndex + 1] + game.rolls[rollIndex + 2]; 
+    } 
+    function getSpareScore() {
+        return game.rolls[rollIndex] + game.rolls [rollIndex + 1] + game.rolls[rollIndex + 2]; 
+    }
+    function getNormalScore() {
+       return game.rolls[rollIndex] + game.rolls[rollIndex+1];
+    }
+};
